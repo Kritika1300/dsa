1cf5b468a348c33a278de067a5d009a292bda5a5
Kritika <kaur.kritika.kritika@gmail.com>
2021-03-26 12:29:01 +0530

    Count leaf nodes

--- Trees/Binary Tree/Count leaf nodes.java
+++ Trees/Binary Tree/Count leaf nodes.java
@@ -19,20 +19,23 @@ public class Main {
         root.left.right = new Node(5);
         System.out.println(countLeafNodes(root));
     }
-    // public static void countLeafNodes(Node root){
-    //     if(root == null) return;
-    //     countLeafNodes(root.left);
-    //     if(root.left == null && root.right == null){
-    //       count++;
-    //     }
-    //     countLeafNodes(root.right);
-    // }
     public static int countLeafNodes(Node root){
         if(root == null) return 0;
-        else if(root.left == null && root.right == null) return 1;
-        int left = countLeafNodes(root.left);
-        int right = countLeafNodes(root.right);
-        return left+right;
+        countLeafNodes(root.left);
+        if(root.left == null && root.right == null){
+        count++;
+        }
+        countLeafNodes(root.right);
+        return count;
     }
+    // public static int countLeafNodes(Node root){
+    //     if(root == null) return 0;
+    //     else if(root.left == null && root.right == null) return 1;
+    //     int left = countLeafNodes(root.left);
+    //     int right = countLeafNodes(root.right);
+    //     return left+right;
+    // }
 }
+    
+
     
